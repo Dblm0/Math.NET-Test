08a3cdb046ae24404e3e68099c91490fdb1a3385
DmitrievDV <13@13.tt>
2020-08-24 16:37:28 +0300

    Add project files.

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MathNet.Numerics.Data.Text;
+using MathNet.Numerics.Data.Matlab;
+using MathNet.Numerics.LinearAlgebra;
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = Matrix<double>.Build.Random(2, 2);
+            DelimitedWriter.Write("123", data);
+            MatlabWriter.Write("123", Matrix<double>.Build.Random(2, 2), "a");
+        }
+    }
+}
